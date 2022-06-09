83744f517f6a49bf01814b1f02c5dd5f9968afa1
GSvenny <gabrielswagner7@gmail.com>
2022-06-09 11:39:20 -0500

    quiz application MVP

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,10 +1,31 @@
 /*Variables*/
-var ul_1 = document.getElementsByClassName('answer1');
-var ul_2 = document.getElementsByClassName('answer2');
-var ul_3 = document.getElementsByClassName('answer3');
+var ul_1 = document.querySelector('.answer1');
+var ul_2 = document.querySelector('.answer2');
+var ul_3 = document.querySelector('.answer3');
 
-var q1 = document.getElementById('questionOne');
-var q2 = document.getElementById('questionTwo');
-var q3 = document.getElementById('questionThree');
+var q1 = document.querySelector('#questionOne');
+var q2 = document.querySelector('#questionTwo');
+var q3 = document.querySelector('#questionThree');
 
-var quiz = document.getElementsByClassName('quiz');
+var quiz = document.querySelector('.quiz');
+var results = document.querySelector('.results');
+
+
+//----- First Question -----//
+ul_1.addEventListener("click", function() {
+    q1.style.display = "none";
+    q2.style.display = "block";
+});
+
+//-----Second Question-----//
+ul_2.addEventListener("click", function() {
+    q2.style.display = "none";
+    q3.style.display = "block";
+});
+
+//-----Results-----//
+ul_3.addEventListener("click", function() {
+    q3.style.display = "none"
+    quiz.style.display = "none"
+    results.style.display = "block"
+});
