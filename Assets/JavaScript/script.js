1dc25845e002e8dd3286c0a8098403f58b8cce4e
GSvenny <gabrielswagner7@gmail.com>
2022-06-09 10:33:03 -0500

    basic quiz layout

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,5 +1,10 @@
-var greetTeam = document.getElementById('click')
-greetTeam.addEventListener("click", greet)
-function greet () {
-    alert("Hey Shane, Zee, Farhan, Larry, and Gabriel!");
-}
+/*Variables*/
+var ul_1 = document.getElementsByClassName('answer1');
+var ul_2 = document.getElementsByClassName('answer2');
+var ul_3 = document.getElementsByClassName('answer3');
+
+var q1 = document.getElementById('questionOne');
+var q2 = document.getElementById('questionTwo');
+var q3 = document.getElementById('questionThree');
+
+var quiz = document.getElementsByClassName('quiz');
