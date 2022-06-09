6bc924fce4a6043f2fa622151dbbd877d4fe663c
GSvenny <gabrielswagner7@gmail.com>
2022-06-09 14:51:41 -0500

    show results

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -9,23 +9,37 @@ var q3 = document.querySelector('#questionThree');
 
 var quiz = document.querySelector('.quiz');
 var results = document.querySelector('.results');
-
+var finalResults = document.querySelector('.finalResults');
+;
 
 //----- First Question -----//
-ul_1.addEventListener("click", function() {
+ul_1.addEventListener("click", function(event) {
     q1.style.display = "none";
     q2.style.display = "block";
+    
+    var response1 = event.target;
+    finalResults.appendChild(response1);
+    
 });
 
+
+
 //-----Second Question-----//
-ul_2.addEventListener("click", function() {
+ul_2.addEventListener("click", function(event) {
     q2.style.display = "none";
     q3.style.display = "block";
+
+    var response2 = event.target;
+    finalResults.appendChild(response2);
 });
 
 //-----Results-----//
-ul_3.addEventListener("click", function() {
+ul_3.addEventListener("click", function(event) {
     q3.style.display = "none"
     quiz.style.display = "none"
     results.style.display = "block"
+
+    var response3 = event.target;
+    finalResults.appendChild(response3);
 });
+
