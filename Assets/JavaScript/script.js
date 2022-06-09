bde5f8064adde097e9b63ddb4a20f2981cfa5f5b
GSvenny <gabrielswagner7@gmail.com>
2022-06-09 17:14:01 -0500

    added local storage to question items

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -7,19 +7,24 @@ var q1 = document.querySelector('#questionOne');
 var q2 = document.querySelector('#questionTwo');
 var q3 = document.querySelector('#questionThree');
 
+
 var quiz = document.querySelector('.quiz');
 var results = document.querySelector('.results');
 var finalResults = document.querySelector('.finalResults');
-;
+var petChoices = [];
 
 //----- First Question -----//
+
 ul_1.addEventListener("click", function(event) {
+   
     q1.style.display = "none";
     q2.style.display = "block";
     
     var response1 = event.target;
     finalResults.appendChild(response1);
+    petChoices.push(response1.innerHTML)
     
+    console.log(response1)
 });
 
 
@@ -31,6 +36,12 @@ ul_2.addEventListener("click", function(event) {
 
     var response2 = event.target;
     finalResults.appendChild(response2);
+    console.log(response2);
+    petChoices.push(response2.innerHTML)
+    // finalResults.appendChild(response2);
+    
+
+
 });
 
 //-----Results-----//
@@ -39,7 +50,15 @@ ul_3.addEventListener("click", function(event) {
     quiz.style.display = "none"
     results.style.display = "block"
 
-    var response3 = event.target;
+
+    response3 = event.target;
     finalResults.appendChild(response3);
+    petChoices.push(response3.innerHTML);
+    
+
+//-----Save to localStorage-----//
+
+    console.log(petChoices)
+localStorage.setItem("petChoices", JSON.stringify(petChoices))
 });
 
