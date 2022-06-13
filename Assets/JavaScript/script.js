c416fbc6750557083105b2ac08d2f48792299234
GSvenny <gabrielswagner7@gmail.com>
2022-06-13 00:31:48 -0500

    reworked API

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -1,4 +1,6 @@
 /*Variables*/
+
+var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJlTXlqdnF5eVd6UTh0N2FEZzBTbzVYRGNXR0N2bXhzSjNxTTN0NlJVSGdidWsyYUNybiIsImp0aSI6IjhkNWI4YjUxMDA2MDI0MzYzZjg0YmY1MzQzOGFlYzc0NTAxYTI4NmI3MDlkMTllNDIzYWUwNmE1ZDc2ZDYwZjA2NjFhMmY2NWExZTU2YzY1IiwiaWF0IjoxNjU1MDkxMjYwLCJuYmYiOjE2NTUwOTEyNjAsImV4cCI6MTY1NTA5NDg2MCwic3ViIjoiIiwic2NvcGVzIjpbXX0.KxcYhLrfQdFqDWS48JkL7nAW0Pt9Q0EcbLzhDp108JwcXXTCwcgQnweV4LOjN81RCb89HCVh0JDFOJ8zNuVwIFXjjhx50Td9qyo6xnzJ1S0Y8iYUjyZmkseCuzX1F-gv2SqPwn1qwRvOqLWsfvM6GgmjCOXmCtYKR4hDQleoGGP3lZJEmHyyie_HfnX93AenIULOZzESBI7P1nAJ2dUcClhHmP-Lcx5tD-T8mx4dolM3UPmEWpWSGVh1M2xv1c7ty31y2z7xbz-8kZhOCdW_9K-Bx06H9tb4S5TrMM18VYkq7Zjuzb3XRbrx7rjApHchjkdgqX3Ct1UwzeXYKccq9g"
 var ul_1 = document.querySelector('.answer1');
 var ul_2 = document.querySelector('.answer2');
 var ul_3 = document.querySelector('.answer3');
@@ -64,9 +66,17 @@ localStorage.setItem("petChoices", JSON.stringify(petChoices))
 
 });
 
-fetch(url) 
+//----API Call----///
+
+
+fetch("https://api.petfinder.com/v2/animals", {
+    headers: {
+        Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJlTXlqdnF5eVd6UTh0N2FEZzBTbzVYRGNXR0N2bXhzSjNxTTN0NlJVSGdidWsyYUNybiIsImp0aSI6ImI3YTZjOWVlODk1YzBiZjJjYjUwYmVhNGUzY2Q3ZTk0ZDU4NWUyZDRkZDk0OWUzMTI4MmRkNmFhMjM1ZjZiZTlmZDY4Y2ZkN2RkYWRhNGRmIiwiaWF0IjoxNjU1MDk4MTk1LCJuYmYiOjE2NTUwOTgxOTUsImV4cCI6MTY1NTEwMTc5NSwic3ViIjoiIiwic2NvcGVzIjpbXX0.BIEvq26L0r5_c7deJoe9BQOW5B2KLanIWqfmTlf0Xg9ssM580gSxyIj0elgZVNFzKY-1-f1jCEheWNSNVAsHMa_ZyOCm0ozF_OSddrlk_KRcYDkDCYJO2jXcltJ4ot0dN-SIQZUHlfNoSow6LXunATEjtGf-ARLrKGRhYp6SlMO5JfNRIqWel3PXrFv5q0ecKAk5g5bRT15QCGW8FuWdIYxbmGrP1s1PLwQUqtBygUvRbwzbsC7F53u06gC3I6TTJVYBwAZrUJBHl7-9JM4aNo5r3bqWyyqHNFuZJHGmPdgGm8IuM4DLgUicqdOZ_fkFIWDOYfYZDpbU6R7bX-noTw", 
+    }
+})
+
 .then(function(response) {
-    return response.JSON();
+    return response.json();
 })
 .then (function (data) {
 console.log(data);
