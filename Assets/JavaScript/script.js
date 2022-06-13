374d4796b47edc7add45d1cf54284b34c23e61fb
GSvenny <gabrielswagner7@gmail.com>
2022-06-13 17:08:09 -0500

    added 4th question (gender)

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -3,14 +3,16 @@
 /*Variables*/
 var clientId = "eMyjvqyyWzQ8t7aDg0So5XDcWGCvmxsJ3qM3t6RUHgbuk2aCrn";
 var secret = "aWV5jwQBJnkFks98afBWwFAl1vrEUNtLtl7ekKRB";
-var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJlTXlqdnF5eVd6UTh0N2FEZzBTbzVYRGNXR0N2bXhzSjNxTTN0NlJVSGdidWsyYUNybiIsImp0aSI6ImE5Njc5MTA1Yjk4NjU0M2RhOTQxYjY0MmQ2ZTA5YWRiNmRmMTAwZjMzOTEyNTlhNGE0NGZmZWQ1ZTlmMjlhZTgyZTJkOTAzNGRlZmYwNDNmIiwiaWF0IjoxNjU1MTMzNDQxLCJuYmYiOjE2NTUxMzM0NDEsImV4cCI6MTY1NTEzNzA0MSwic3ViIjoiIiwic2NvcGVzIjpbXX0.z03UHjOWAe1eCWdFS-TpqUXltXVnr_cAQmBOMleBhUQH6tbZlPDf54-WvfkTrGDeuM5hySAodZojDALlghHD8cOF7F9hNhtL1JmIDkNk4wo7-NxIt6ar66gCWZD9iqF0PfJjxKogkIE_k4t79jq6SWBF68B4yhDvjGxnQB_wT7cU0ZwPWMTt_4FSfNq7x8_nrVjR96hbV6tSdkbd4r0di_TujApuhmA3DRBG4CBBceFw7BGYRY7NcNwy2FlSkAMYuhuqmDGax0Jn6JqCmwYaTTKDMu00XX3S3A5SS7z0ns5Xg5mrUDQD20YGF_qT3djE-9Bwlqx6PUAiZP6GFXNuGg";
+var token = "";
 var ul_1 = document.querySelector('.answer1');
 var ul_2 = document.querySelector('.answer2');
 var ul_3 = document.querySelector('.answer3');
+var ul_4 = document.querySelector('.answer4');
 
 var q1 = document.querySelector('#questionOne');
 var q2 = document.querySelector('#questionTwo');
 var q3 = document.querySelector('#questionThree');
+var q4 = document.querySelector('#questionFour');
 
 
 var quiz = document.querySelector('.quiz');
@@ -18,6 +20,7 @@ var results = document.querySelector('.results');
 var finalResults = document.querySelector('.finalResults');
 var petChoices = [];
 
+/*Get New Token */
    fetch("https://api.petfinder.com/v2/oauth2/token", {
   body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`,
   headers: {
@@ -31,6 +34,9 @@ var petChoices = [];
    .then(function (data) {
     token = data.access_token;
    })
+
+
+
 
 //----- First Question -----//
 
@@ -63,28 +69,40 @@ ul_2.addEventListener("click", function(event) {
 
 });
 
-//-----Results-----//
+//-----Third Question-----//
 ul_3.addEventListener("click", function(event) {
-    q3.style.display = "none"
-    quiz.style.display = "none"
-    results.style.display = "block"
-    event.preventDefault();
-
+    q3.style.display = "none";
+    q4.style.display = "block";
+  
 
     response3 = event.target;
     finalResults.appendChild(response3);
     console.log(response3)
     petChoices.push(response3.innerHTML);   
 
+});
+
+ul_4.addEventListener("click", function(event) {
+    q4.style.display = "none"
+    quiz.style.display = "none"
+    results.style.display = "block"
+    event.preventDefault();
+
+    response4 = event.target;
+    finalResults.appendChild(response4);
+    console.log(response4);
+    petChoices.push(response4.innerHTML);
+
+
+
 //-----Save to localStorage-----//
 
-    // console.log(petChoices)
 localStorage.setItem("petChoices", JSON.stringify(petChoices))
 
 //----API Call----///
 
-console.log(petChoices[0])
-fetch(`https://api.petfinder.com/v2/animals?type=${petChoices[0]}&age=${petChoices[1]}&size=${petChoices[2]}`, {
+console.log(petChoices)
+fetch(`https://api.petfinder.com/v2/animals?type=${petChoices[0]}&age=${petChoices[1]}&size=${petChoices[2]}&gender=${petChoices[3]}`, {
     headers: {
         Authorization: `Bearer ${token}`,
     }
@@ -123,7 +141,5 @@ petLink.appendChild(link);
 petLink.title = "Adopt Me";
 petLink.href = data.animals[0].url;
 finalResults.append(petLink);
-
-
-})
+});
 });
